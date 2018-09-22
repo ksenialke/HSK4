651d29229f2659a2185d8255b06c1d6aa4cb20df
Hubert Barc <barchubert@gmail.com>
2018-09-22 18:16:36 +0200

    Nowe rzeczy se dodalem

--- app.js
+++ app.js
@@ -6,48 +6,31 @@ const path = require('path');
 // To use HTTP and client
 const http = require('http');
 
-// Routes
-// app.get(path, callback)
-// The first argument (path) is anything that comes after the domain
-// The second argument is  a callback function that tells the server what to do when the path is matched
-// __dirname is directory that contains the JavaScript source codenpm install nodemon --save-dev
+app.set('view engine', 'ejs');
+app.use('/css', express.static('css'));
+
+let words = 36;
+
+// Returns unknown words
+function getUnknown(words){
+    return 1200-(words);
+}
+
+//Returns the percentage
+function percentage(words){
+    return ((words/1200) * 100);
+}
+
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-app.get('/javascript', (req, res) => {
-    res.sendFile(path.join(__dirname, '/app.js'));
-});
-
-app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, '/style.css'));
+    res.render('index', {
+        percentage: percentage(words) + "%",
+        known_words: words,
+        words_left: getUnknown(words)
+    });
 });
 
 // Creating the server where browsers can connect
 app.listen(8000, () => {
     console.log('App is starting at port', 8000)
 });
-
-
-let words = 36;
-
-// Returns known words
-function getWords(words) {
-    return words;
-};
-
-// Returns unknown words
-function getUnknown(){
-    return 1200-getWords(words);
-}
-
-//Returns the percentage
-function percentage(words){
-    return ((words/1200) * 100).toFixed(2);
-}
-
-
-console.log('Known:'+getWords(words));
-console.log('Unknown:'+getUnknown(getWords(words)));
-console.log('Percentage: '+percentage(words)+'%')
